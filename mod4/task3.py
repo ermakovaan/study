39b5ba8fcce25d399994531d6f57a268ccae37a2
ermakovaan <145872368+ermakovaan@users.noreply.github.com>
2023-12-22 23:23:22 +0500

    Delete mod4/task3.py

--- mod4/task3.py
+++
@@ -1,8 +0,0 @@
-def nod(x, y):
-    while(y):
-        x, x = y, x % y
-        return x
-x = int(input("Введите первое число: "))
-y = int(input("Введите второе число: "))
-numbers = nod(x, y)
-print("НОД двух чисел равен:", numbers)
